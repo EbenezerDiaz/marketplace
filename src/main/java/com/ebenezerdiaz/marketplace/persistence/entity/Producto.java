185100ce9e63caef38f3a12cc30a848c73d2df63
Ebenezer Diaz <diaz.ebenezer@gmail.com>
2021-06-02 09:17:15 -0500

    Se agrega Producto y Compra

--- src/main/java/com/ebenezerdiaz/marketplace/persistence/entity/Producto.java
+++ src/main/java/com/ebenezerdiaz/marketplace/persistence/entity/Producto.java
@@ -1,4 +1,39 @@
 package com.ebenezerdiaz.marketplace.persistence.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "productos")
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "codigo_barras")
+    private String codigoBarras;
+
+    @Column(name = "precio_venta")
+    private Double previoVenta;
+
+    @Column(name = "cantidadStock")
+    private Integer cantidadStock;
+
+    @Column(name = "estado")
+    private Boolean estado;
+
+
 }
